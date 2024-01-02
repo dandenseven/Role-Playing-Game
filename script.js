41ddc539ad1187bdd2d3ad75a24de382338c1561
Dane knight <knight1seven@gmail.com>
2024-01-01 19:26:28 -0500

    function update

--- script.js
+++ script.js
@@ -24,13 +24,7 @@ button2.onclick = goCave;
 button3.onclick =fightDragon;
 
 function goTown() {
-    button1.innerText = "Go to store";
-    button2.innerText = "Go to cave";
-    button3.innerText = "Fight dragon";
-    button1.onclick = goStore;
-    button2.onclick = goCave;
-    button3.onclick = fightDragon;
-    text.innerText = "You are in the town square. You see a sign that says \"Store\".";
+    update(locations[0]);
 }
 
 function goStore() {
@@ -60,7 +54,13 @@ function buyWeapon() {
 }
 
 function update(location) {
-
+    button1.innerText = location["button text"][0];
+    button2.innerText = location["button text"][1];
+    button3.innerText = location["button text"][2];
+    button1.onclick = goStore;
+    button2.onclick = goCave;
+    button3.onclick = fightDragon;
+    text.innerText = "You are in the town square. You see a sign that says \"Store\".";
 }
 
 const locations = [

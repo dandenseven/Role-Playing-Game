b88d37e1711f6669248f4a3575daa9db14240df0
Dane knight <knight1seven@gmail.com>
2024-01-02 15:46:39 -0500

    added third element to locations

--- script.js
+++ script.js
@@ -28,17 +28,11 @@ function goTown() {
 }
 
 function goStore() {
-    button1.innerText = "Buy 10 health (10 gold)";
-    button2.innerText = "Buy weapon (30 gold)";
-    button3.innerText = "Go to town square";
-    text.innerText = "You enter the store.";
-    button1.onclick = buyHealth;
-    button2.onclick = buyWeapon;
-    button3.onclick = goTown;
+    update(locations[1])
 }
 
 function goCave() {
-    console.log("Going to cave.");
+    update(locations[2])
 }
 
 function fightDragon() {
@@ -50,6 +44,15 @@ function buyHealth() {
 }
 
 function buyWeapon() {
+    gold = gold - 10;
+    health - health - 10;
+}
+
+function fightSlime() {
+
+}
+
+function fightBeast() {
 
 }
 
@@ -57,11 +60,10 @@ function update(location) {
     button1.innerText = location["button text"][0];
     button2.innerText = location["button text"][1];
     button3.innerText = location["button text"][2];
-    button1.onclick = goStore;
-    button2.onclick = goCave;
-    button3.onclick = fightDragon;
-    text.innerText = "You are in the town square. You see a sign that says \"Store\".";
-}
+    button1.onclick = location["button functions"][0];
+    button2.onclick = location["button functions"][1];
+    button3.onclick = location["button functions"][2];
+    text.innerText = location.text
 
 const locations = [
     {
@@ -73,6 +75,15 @@ const locations = [
         name: "store",
         "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
         "button functions": [buyHealth, buyWeapon, goTown],
-        text: "You enter the store."
+        text: "You enter the store."},
+
+    {
+        name: "cave",
+        "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+        "button functions": [fightSlime, fightBeast, goTown],
+        text: "You enter the cave. You see some monsters."
     }
-];                                                                                                                                                                                                    
+
+];
+
+}

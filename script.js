9021010e61c9e80e3450320d25b8a717115841ca
Dane knight <knight1seven@gmail.com>
2024-01-03 15:03:11 -0500

    updated buyweapon function and add function sellWeapon

--- script.js
+++ script.js
@@ -18,6 +18,34 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+const weapons = [
+    { name: "stick", power: 5},
+    { name:"dagger", power: 30},
+    { name:"claw hammer", power: 50},
+    { name:"sword", power: 100}
+    
+];
+
+const locations = [
+    {
+        name: "town sqaure",
+        "button text": ["Go to store", "Go to cave", "Fight dragon"],
+        "button functions": [goStore, goCave, fightDragon],
+        text: "You are in the town square. You see a sign that says \"Store\"."},
+    {
+        name: "store",
+        "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+        "button functions": [buyHealth, buyWeapon, goTown],
+        text: "You enter the store."},
+
+    {
+        name: "cave",
+        "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+        "button functions": [fightSlime, fightBeast, goTown],
+        text: "You enter the cave. You see some monsters."
+    }
+
+];
 // intialize buttons
 button1.onclick = goStore;
 button2.onclick = goCave;
@@ -50,10 +78,31 @@ function buyHealth() {
     }
 }
 
+
 function buyWeapon() {
-    if ( gold >= 30); {
-        
+    if (currentWeapon < weapons.length - 1) {
+        if (gold >= 30) {
+        gold -= 30;
+        currentWeapon++;
+        goldText.innerText = gold;
+        let newWeapon = weapons[currentWeapon].name;
+        text.innerText = "You now have a " + newWeapon + ".";
+        inventory.push(newWeapon);
+        text.innerText += " In your inventory you have: " + inventory;
+        } else { text.innerText = "You do not have enough gold to buy a weapon.";}
+    } else { 
+        text.innerText = "You already have the most powerful weapon!";
+        button2.innerText = "sell weapon for 15 gold";
+        button2.onclick = sellWeapon;
     }
+    
+}
+
+
+function sellWeapon() {
+    if (inventory.length > 1);
+    gold += 15;
+    goldText.innerText = gold;
 }
 
 function fightSlime() {
@@ -72,35 +121,6 @@ function update(location) {
     button2.onclick = location["button functions"][1];
     button3.onclick = location["button functions"][2];
     text.innerText = location.text
-
-const weapons = [
-    { name: "stick", power: 5},
-    { name:"dagger", power: 30},
-    { name:"claw hammer", power: 50},
-    { name:"sword", power: 100}
-    
-];
-
-const locations = [
-    {
-        name: "town sqaure",
-        "button text": ["Go to store", "Go to cave", "Fight dragon"],
-        "button functions": [goStore, goCave, fightDragon],
-        text: "You are in the town square. You see a sign that says \"Store\"."},
-    {
-        name: "store",
-        "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
-        "button functions": [buyHealth, buyWeapon, goTown],
-        text: "You enter the store."},
-
-    {
-        name: "cave",
-        "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
-        "button functions": [fightSlime, fightBeast, goTown],
-        text: "You enter the cave. You see some monsters."
-    }
-
-];
 
 }
 

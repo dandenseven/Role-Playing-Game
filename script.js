b28a3bf549846419e98153cc069e0c23fce5badd
Dane knight <knight1seven@gmail.com>
2024-01-05 23:38:05 -0500

    added defeatMonster function and another object to locations array

--- script.js
+++ script.js
@@ -55,8 +55,14 @@ const locations = [
         "button text": ["Attack", "Dodge", "Run"],
         "button functions": [attack, dodge, goTown],
         text: "You are fighting a monster."
-    
+    },
+    {
+        name: "kill monster",
+        "button text":["Go to town square", "Go to town square", "Go to town square" ],
+        "button functions": [goTown, goTown, goTown],
+        text: "The monster screams Arg! as it dies. You gain experience points and find gold."
     }
+    
 
 ];
 // intialize buttons
@@ -151,12 +157,37 @@ function goFight() {
     update(locations[3]);
     monsterHealth = monsters[fighting].health;
     monsterStats.style.display = "block";
+    monsterName.innerText = monsters[fighting].name;
+    monsterHealthText.innerText = monsters[fighting].health;
 }
 
 function attack() {
-
+    text.innerText = "The " + monsters[fighting].name + " attacks.";
+    text.innerText = " You attack it with your " + weapons[currentWeapon].name + ".";
+    health -= monsters[fighting].level;
+    monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+    healthText.innerText = health;
+    monsterHealthText.innerText = monsterHealth;
+    if (health <= 0) {
+        local();
+    } else if (monsterHealth <= 0) {
+        defeatMonster()
+    }
 }
 
+
 function dodge() {
+    text.innerText = "You dodge the attack from the " + monsters[fighting].name + ".";
+}
+
+function defeatMonster() {
+    gold += Math.floor(monsters[fighting].level * 6.7);
+    xp += monsters[fighting].level;
+    goldText.innerText = gold;
+    xpText.innerText = xp;
+    update(locations[4]);
+}
+
+function lose() {
 
 }
